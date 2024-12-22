d5b2bfcf6a43657cb5119275d2433558c9c1fab4
Kartik <Kartik@bisht4.com>
2024-12-22 06:36:31 +0530

    delete file

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, url_for, flash, session
 from werkzeug.utils import redirect
 from flask_mysqldb import MySQL
+from datetime import date
 
 app = Flask(__name__)
 app.secret_key = 'many random bytes'
@@ -13,22 +14,13 @@ app.config['MYSQL_DB'] = 'student_management1'
 mysql = MySQL(app)
 app.secret_key = "super secret key"
 
-@app.route('/home')
+@app.route('/index')
 def home():
      cur = mysql.connection.cursor()
      cur.execute("SELECT * FROM students")
      data = cur.fetchall()
      cur.close()
-     return render_template('home.html',username= session['username'] ,students=data)
-
-@app.route('/')
-def Index():
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM students")
-    data = cur.fetchall()
-    cur.close()
-
-    return render_template('home.html', students=data)
+     return render_template('home.html' ,students=data)
 
 @app.route('/insert', methods = ['POST'])
 def insert():
@@ -89,7 +81,6 @@ def update():
         return redirect(url_for('home'))
                         
 
-
 @app.route('/studentlogin')
 def student_login():        
     return render_template('login2.html')
@@ -106,15 +97,71 @@ def admin_login():
         if record:
              session['loggedin']= True
              session['username']= record[1]
-             return redirect(url_for('home'))
+             return redirect(url_for('back'))
         else:
             return 'INVALID USERNAME OR PASSWORD'
     return render_template('login.html')
 
-@app.route('/index')
+@app.route('/admin')
+def back():
+    return render_template('back.html')
+
+@app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/index2')
+def index2():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM students")
+    students = cur.fetchall()
+    cur.close()
+    return render_template('index1.html', students=students)
 
-if __name__ == "__main__":
-     app.run(debug=True)
+@app.route('/attendance', methods=['GET','POST'])
+def attendance():
+    student_ids = request.form.getlist('attendance')
+    today = date.today()
+    cur = mysql.connection.cursor()
+    cur.execute("DELETE FROM attendance WHERE date = %s", [today])
+    for student_id in student_ids:
+        cur.execute("INSERT INTO attendance (student_id, date, status) VALUES (%s, %s, %s)",(student_id, today, 'Present'))
+    cur.execute("SELECT id FROM students")
+    all_students = cur.fetchall()
+    for student in all_students:
+        if str(student[0]) not in student_ids:
+            cur.execute("INSERT INTO attendance (student_id, date, status) VALUES (%s, %s, %s)",(student[0], today, 'Absent'))
+    mysql.connection.commit()
+    cur.close()
+    return redirect(url_for('index2'))
+
+
+@app.route('/view_attendance', methods=['GET', 'POST'])
+def view_attendance():
+    cur = mysql.connection.cursor()
+    if request.method == 'POST':
+        selected_date = request.form.get('date')
+        cur.execute("""
+            SELECT students.name, attendance.status 
+            FROM attendance 
+            INNER JOIN students ON attendance.student_id = students.id 
+            WHERE attendance.date = %s
+        """, [selected_date])
+        records = cur.fetchall()
+        cur.close()
+        return render_template('attendance.html', records=records, selected_date=selected_date)
+    today = date.today()
+    cur.execute("""
+        SELECT students.name, attendance.status 
+        FROM attendance 
+        INNER JOIN students ON attendance.student_id = students.id 
+        WHERE attendance.date = %s
+    """, [today])
+    records = cur.fetchall()
+    cur.close()
+    return render_template('attendance.html', records=records, selected_date=today)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
